2735adf40f39808ecc40a62b5c8cf6235b213080
Adam Bruce <adam@adambruce.net>
2021-10-28 21:31:34 +0100

    Clang Tidy

--- src/asm/multiboot_header.asm
+++ src/asm/multiboot_header.asm
@@ -4,7 +4,7 @@ header_start:
     MULTIBOOT_PAGE_ALIGN    equ 1           ; Align to 4K boundaries
     MULTIBOOT_HEADER_ARCH   equ 0           ; 32-bit (protected) mode of i386
     MULTIBOOT_HEADER_MAGIC  equ 0xE85250D6  ; Magic number
-    MULTIBOOT_CHECKSUM      equ - (MULTIBOOT_HEADER_MAGIC + (header_end - header_start)) ; Checksum                              
+    MULTIBOOT_CHECKSUM      equ - (MULTIBOOT_HEADER_MAGIC + (header_end - header_start))                               
     MULTIBOOT_TYPE          equ 0
     MULTIBOOT_FLAGS         equ 0
     MULTIBOOT_SIZE          equ 8
