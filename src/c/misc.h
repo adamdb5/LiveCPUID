d3a338e4a422d4acdae74575d5714903452296f9
Adam Bruce <adam@adambruce.net>
2021-10-30 17:21:18 +0100

    Removed ununsed comment

--- src/c/misc.h
+++ src/c/misc.h
@@ -1,14 +1,6 @@
 #ifndef MISC_H
 #define MISC_H
 
-/**
- *   cpuid->misc.byte_brand_id = ebx & 0b00000000000000000000000011111111;
-  cpuid->misc.clflush_size = ebx & 0b00000000000000001111111100000000;
-  cpuid->misc.logical_processor_count =
-      ebx & 0b00000000111111110000000000000000;
-  cpuid->misc.local_apcid_id = ebx & 0b11111111000000000000000000000000;
-  */
-
 #define MASK_BYTE_BRAND_ID 0x000000FF
 #define MASK_CLFLUSH 0X0000FF00
 #define MASK_LOGICAL_PROC_COUNT 0x00FF0000
